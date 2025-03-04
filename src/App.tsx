00f9113018e3a035c71f40a48b41425faa684456
Murilo Braga <murilobraga3@gmail.com>
2025-03-04 16:51:48 -0300

    fix; corrigindo um bug gerado pelo merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ThemeProvider } from 'styled-components'
 import Cabecalho from './components/Cabecalho'
 import Hero from './components/Hero'
@@ -17,23 +16,6 @@ function App() {
         <ListaVagas />
       </Container>
     </ThemeProvider>
-=======
-import Header from './components/Cabecalho'
-import Hero from './components/Hero'
-import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
-
-function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <div className="container">
-        <ListaVagas />
-      </div>
-    </>
->>>>>>> 6b75ea68847c2a67da4aa2679f111c99ca9da97a
   )
 }
 
